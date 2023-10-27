f425788a1b57f0f407d0b32463c896143e8a2094
Dmucci <dmucci93@gmail.com>
2023-10-27 13:45:59 +0200

    finished

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 let score = document.querySelector("#ratings-range")
-score.addEventListener('click', function (event){
+
+score.addEventListener('click', function (event) {
     document.querySelector("#buttonSubmit").disabled = false;
 
-let submit = document.querySelector("#buttonSubmit");
+    let submit = document.querySelector("#buttonSubmit");
+  
+    let result = Number(event.target.textContent);
+    document.querySelector("#selected-rating").textContent = result
 
-submit.addEventListener('click', function (event){
-console.log(event.target.id);
+    submit.addEventListener('click', function (event) {
+        document.querySelector("#backside-component").style.display= "block";
+        document.querySelector("#frontside-component").style.display = "none";
+        
+    })
 })
 
-
-
-})
